31de23c789ffff5328324cc9b6ea784ec1a9f575
dunjajv <dunjavulliein@gmail.com>
2018-11-25 10:25:13 +0100

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,35 @@
+
+
+print('Hello, Django girls!')
+if 3>2:
+    print('It works!')
+name='Sonja'
+if name=='Ola':
+    print('Hey Ola')
+
+elif name == 'Sonja':
+    print('Hey Sonja!')
+else:
+    print('Hey anonymous!')
+volume = 57
+if volume < 20:
+    print("C'est plutôt calme.")
+elif 20<=volume<40:
+    print("Une jolie musique de fond.")
+elif 40<=volume<60:
+    print("Parfait, je peux entendre tous les détails du morceau.")
+elif 60<=volume<80:
+    print("parfait pour faire la fête")
+elif 80<=volume<100:
+    print("un peu trop fort")
+else:
+    print("au secours mes oreilles :(")
+def hi():
+    print('Hi there!')
+    print('How are you?')
+
+girls = ['Rocio', 'Ebba', 'Rasika', 'Dunja']
+for name in girls:
+        print('Hi ' + name +'!')
+for i in range(1,6):
+    print(i)
